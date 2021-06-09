9c00fcadbd8cdb8a569cf2ff2a9980714d9ff9aa
Ayane Satomi <chinodesuuu@gmail.com>
2021-06-09 18:56:10 +0800

    add initial Primitive layout
    Nothing exists yet here but this is basically just to draw the implementation scope for now
    
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

---
+++ osu.Framework/Primitives/Composite.cs
@@ -0,0 +1,11 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Primitives
+{
+    public abstract class Composite<TComponent>: IComponent, IComposite<TComponent>
+    where TComponent : IComponent
+    {
+        
+    }
+}
