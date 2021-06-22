5794d90e4ac6efc9ae2939d1b4d86fe108c44068
Nathan Alo <nathan.alo2000@gmail.com>
2021-06-22 15:39:19 +0800

    move IUpdateable to Primitives namespace

--- osu.Framework/Primitives/IUpdateable.cs
+++ osu.Framework/Primitives/IUpdateable.cs
@@ -1,10 +1,16 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-namespace osu.Framework
+using osu.Framework.Timing;
+
+namespace osu.Framework.Primitives
 {
     public interface IUpdateable
     {
+        IFrameBasedClock Clock { get; set; }
+
+        FrameTimeInfo Time { get; }
+
         void Update();
     }
 }
